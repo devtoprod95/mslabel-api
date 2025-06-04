096545e4830e8f2e69b66eaf46ef4cb5e3242e73
junyeob kim <junyeob.kim@onch3.co.kr>
2025-06-04 12:59:00 +0900

    로그인 기능

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -17,8 +17,16 @@ class SmsController extends Controller
 
     public function login()
     {
-        $req = $this->request->all();
-        channelLog(json_encode($req, JSON_UNESCAPED_UNICODE), 'login');
+        $req     = $this->request->headers->all();
+        $headers = $this->request->all();
+
+        $logData = [
+            'request_data' => $req,
+            'headers'      => $headers,
+            'method'       => $this->request->getMethod(),
+            'url'          => $this->request->fullUrl()
+        ];
+        channelLog(json_encode($logData, JSON_UNESCAPED_UNICODE), 'login');
 
         return helpers_json_response(HttpConstant::OK);
     }
